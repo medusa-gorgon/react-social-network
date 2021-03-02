4085b91e70ce98d16b180cc36011ad024bd51f8b
Maria Shevchuk <mg.bizarre@gmail.com>
2021-03-02 20:36:58 +0300

    creating redux: add observer pattern model, callback rerender

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from '../render';
+let rerenderEntireTree = () => {
+  console.log('state changed');
+};
 
 let state = {
   profilePage: {
@@ -85,9 +87,13 @@ export let addPost = () => {
   rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
+};
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
 };
 
 export default state;
