ada03bfe0817e0c20ce9804ebe4ad1375fd372f6
Maria Shevchuk <mg.bizarre@gmail.com>
2021-05-27 15:18:22 +0300

    Updates

--- static/js/5.ee710be4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[5],{302:function(t,n,s){},309:function(t,n,s){"use strict";s.r(n);var e=s(302),i=s.n(e),a=s(0);n.default=function(){return Object(a.jsx)("div",{className:i.a.settings,children:"Settings"})}}}]);
-//# sourceMappingURL=5.ee710be4.chunk.js.map
